bb56f0f401ac967500d63a40e6cd56644c0adc43
Reberth <54030471+reberthkss@users.noreply.github.com>
2019-11-20 15:53:13 -0300

    Update theme.cpp

--- src/libsync/theme.cpp
+++ src/libsync/theme.cpp
@@ -320,8 +320,8 @@ QString Theme::gitSHA1() const
         "https://github.com/owncloud/client/commit/"));
     const QString gitSha1(QLatin1String(GIT_SHA1));
     devString = QCoreApplication::translate("ownCloudTheme::about()",
-        "&lt;p&gt;&lt;small&gt;Construído a partir de revisão Git &lt;a href=&quot;%1&quot;&gt;%2&lt;/a&gt;"
-        " em %3, %4 usando Qt %5, %6&lt;/small&gt;&lt;/p&gt;")
+        "<p><small>Construído a partir de revisão Git <a href=%1 >%2</a>"
+        " em %3, %4 usando Qt %5, %6</small></p>")
                     .arg(githubPrefix + gitSha1)
                     .arg(gitSha1.left(6))
                     .arg(__DATE__)
